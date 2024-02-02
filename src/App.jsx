7593c4496bce5828e038e26ae71e180cb79ecb22
Aditya Rawat <adityarawat2801@gmail.com>
2024-02-02 12:47:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import PhoneOtpForm from './components/phone-login'
+import './App.css'
+
+function App() {
+ return (
+    <div className='App'>
+     <h1> Login With Phone </h1>
+    <PhoneOtpForm />
+      </div>
+ ) 
+};
+
+export default App

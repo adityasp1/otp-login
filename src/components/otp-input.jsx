2b6d40b1dae0acc3bd63dba63d156279c7cde3fa
Aditya Rawat <adityarawat2801@gmail.com>
2024-02-02 13:48:06 +0530

    otp-field

--- src/components/otp-input.jsx
+++ src/components/otp-input.jsx
@@ -1,15 +1,55 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 
 const OtpInput = ({length = 4, onOtpSubmit = () => { }}) => {
 
     const [otp, setOtp] = useState(new Array(length).fill(""));
 
-    console.log(otp);
+    const inputRefs = useRef([])
 
-    const handleChange = ()=> {}
-    const handleClick = ()=> {}
-    const handleKeyDown = ()=> {}
+    useEffect(() => {
+   if(inputRefs.current[0]) {
+    inputRefs.current[0].focus();
+   }
+    },[])
+
+
+
+    const handleChange = (index,e)=> {
+        const value = e.target.value
+        if(isNaN(value)) return
+
+        const newOtp = [...otp];
+
+        // allow one input only
+        newOtp[index] = value.substring(value.length -1);
+        setOtp(newOtp)
+
+        // submit trigger
+        const combinedOtp = newOtp.join(""); 
+        if(combinedOtp.length === length) onOtpSubmit(combinedOtp);
+
+
+        // move to next field if current field is filled
+        if(value && index < length -1 && inputRefs.current[index+1]){
+            inputRefs.current[index + 1].focus();
+        }
+    }
+    const handleClick = (index)=> {
+        inputRefs.current[index].setSelectionRange(1,1)
+
+        // additional validation 
+        if(index > 0 && !otp[index -1 ]) {
+        inputRefs.current[otp.indexOf("")].focus();
+        }
+    }
+    const handleKeyDown = (index,e)=> {
+        if(e.key === "Backspace" && !otp[index] && index>0 &&
+        inputRefs.current[index-1]) {
+            // move focus back to previous field on backspace
+          inputRefs.current[index -1 ].focus();  
+        }
+    }
  
     return <div>
         {
@@ -17,6 +57,7 @@ const OtpInput = ({length = 4, onOtpSubmit = () => { }}) => {
               return <input 
               key={index}
               type="text"
+              ref={(input) => inputRefs.current[index] = input}
               value={value}
               onChange={(e)=>handleChange(index,e)}
               onClick={() => handleClick(index)}
